8363be3cb9a17ab13d1793322f047d37f7d55577
don2101 <don210198@gmail.com>
2019-10-17 11:34:36 +0900

    S1P1351023-27 회원가입 기능 구현
    - password를 hashing 하기 위해 dict로 변환 후 다시 QueryDict 객체로
    변환하여 적용
    - User 객체를 먼저 생성하고, UserProfile 객체를 생성한 후 Uesr와 연결

--- back-end/account/views.py
+++ back-end/account/views.py
@@ -1,20 +1,33 @@
 from django.shortcuts import render
+from django.contrib.auth.hashers import make_password, is_password_usable
+from django.http import QueryDict
+
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .serializers import UserSerializer, BabySerializer
+
+from .serializers import UserSerializer, UserProfileSerializer, BabySerializer
 
 # Create your views here.
 
 @api_view(["POST"])
 def signup(request):
-    
-    serializer = UserSerializer(data=request.data, partial=True)
-    
-    if serializer.is_valid():
-        serializer.save()
-    
-    
+    # querydict is immutable. convert it to dict and update password, then recover it back to querydict
+    request_dict = request.data.dict()
+    request_dict['password'] = make_password(request.data['password'])
+    modified_data = QueryDict('', mutable=True)
+    modified_data.update(request_dict)
 
+    user_serializer = UserSerializer(data=modified_data, partial=True)
+    
+    if user_serializer.is_valid():
+        user_instance = user_serializer.save()
+        
+        profile_serializer = UserProfileSerializer(data=request.data, partial=True)
+        
+        if profile_serializer.is_valid():
+            profile_instance = profile_serializer.save(user=user_instance)
+
+        
 
 def login(request):
     pass
