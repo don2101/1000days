e3790e5cfa5266e8274ed2ccc3ff1366f79fc45d
don2101 <don210198@gmail.com>
2019-10-17 11:30:33 +0900

    S1P1351023-27 Test코드 작성, admin 관리 모델 추가
    - 회원가입에 대한 test 코드 작성
    - requesets 사용하여 test 진행
    - Baby, UserProfile 모델을 admin 관리 페이지에 추가

--- back-end/account/tests.py
+++ back-end/account/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+import requests
 
 # Create your tests here.
+
+class AccountTest(TestCase):
+    def setUp(self):
+        self.signUpUrl = "http://localhost:8000/account/signup/"
+        self.signUpData = {
+            "username": "test12",
+            "password": "12345",
+            "email": "qwe123@qwe.com",
+            "nickname": "testing123",
+            "select_baby": True,
+            "account_open": True,
+            "follower_open": True
+        }
+
+    def test_signup(self):
+        result = requests.post(self.signUpUrl, self.signUpData)
