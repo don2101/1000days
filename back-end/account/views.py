47ca286d50e2654efd78df63014f82455ec189f6
don2101 <don210198@gmail.com>
2019-10-17 17:43:31 +0900

    Django Swagger 적용하여 API 문서 생성
    - django-rest-swagger 적용
    - settings.py에 관련 설정 추가
    - 메인 urls.py에서 doc문서에 접근하는 url 추가
    - 각 함수마다 문서에 출력될 상세 내용 추가

--- back-end/account/views.py
+++ back-end/account/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.contrib.auth.hashers import make_password, is_password_usable
 
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, renderer_classes
 from rest_framework import status
+from rest_framework_swagger import renderers
 
 from .serializers import UserSerializer, UserProfileSerializer, BabySerializer
 from .account_service import create_token, user_authenticate, set_password
@@ -12,7 +13,21 @@ from .account_service import create_token, user_authenticate, set_password
 # Create your views here.
 
 @api_view(["POST"])
+@renderer_classes([renderers.OpenAPIRenderer, renderers.SwaggerUIRenderer])
 def signup(request):
+    """
+    회원 가입을 요청하는 API
+    ---
+    ## post parameter
+        username: 사용자의 이름(String),
+        password: 사용자의 비밀번호(String),
+        email: 사용자의 email(String),
+        nickname: 사용자의 계정 이름(String),
+        select_baby: 아기 존재 여부(Boolean),
+        account_open: 계정 정보 공개 여부(Boolean),
+        follower_open: Follower 정보 공개 여부(Boolean)
+    ---
+    """
     modified_data = set_password(request)    
     
     user_serializer = UserSerializer(data=modified_data, partial=True)
@@ -34,6 +49,14 @@ def signup(request):
 
 @api_view(["POST"])
 def login(request):
+    '''
+    회원 가입을 요청하는 API
+    ---
+    ## post parameter
+        email: 사용자의 email(String),
+        password: 사용자의 비밀번호(String),
+    ---
+    '''
     authenticated = user_authenticate(request.data["email"], request.data["password"])
     
     if authenticated:
