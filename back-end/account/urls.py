471a86bf9ecb18e0c865ac5bd9c6dd92c9ea8b06
don2101 <don210198@gmail.com>
2019-10-18 13:39:16 +0900

    S1P1351023-50 url 수정
    - baby 정보 요청에서 id가 아닌 url로 접근하도록 변경

--- back-end/account/urls.py
+++ back-end/account/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import signup, login, personal, baby
+from .views import signup, login, personal, babies
 
 
 urlpatterns = [
     path('signup/', signup, name="signup"),
     path('login/', login, name="login"),
     path('<str:account_name>', personal, name="personal"),
-    path('<str:account_name>/<int:baby_id>', baby, name="personal"),
+    path('<str:account_name>/babies/', babies, name="babies"),
 ]
