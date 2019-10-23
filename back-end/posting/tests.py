04a3b4ed14c2ac2e7a75b829fe964929c48c2afb
don2101 <don210198@gmail.com>
2019-10-23 15:53:36 +0900

    S1P1351023-60 Image 관련 기능 추가
    - admin.py에 Image 관리용 모델 추가
    - Image 관련 testing 구현

--- back-end/posting/tests.py
+++ back-end/posting/tests.py
@@ -18,19 +18,30 @@ class DiaryTest(TestCase):
             "email": "user987@user.com",
             "password": "12345"
         }
+
+        self.imageUrl = "http://localhost:8000/diary/testing/"
+        self.imageData = {
+            'image_array': [{'image': './testing1.gif'}, {'image': './testing2.gif'}]
+        }
     
 
-    def Test_login(self):
-        result = requests.post(self.loginUrl, self.loginData)
+    # def Test_login(self):
+    #     result = requests.post(self.loginUrl, self.loginData)
 
-        self.assertEqual(result.status_code, 200)
+    #     self.assertEqual(result.status_code, 200)
         
-        return result
+    #     return result
 
-    def test_post(self):
-        result = self.Test_login()
-        token = result.json()
-        self.post_body['token'] = token['token']
+    # def test_post(self):
+    #     result = self.Test_login()
+    #     token = result.json()
+    #     self.post_body['token'] = token['token']
 
-        response = requests.post(self.postUrl, self.post_body)
+    #     response = requests.post(self.postUrl, self.post_body)
+    #     print(response)
+
+    def test_image(self):
+        headers = {'Content-Type': 'application/json; charset=utf-8'}
+
+        response = requests.post(url=self.imageUrl, headers=headers, data=self.imageData)
         print(response)
