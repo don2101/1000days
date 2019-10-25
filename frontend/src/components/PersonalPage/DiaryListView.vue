a372eb3a8ee985f647e63cb1db1514cf7b568055
Lee Ah Rum <lar404@naver.com>
2019-10-25 13:50:03 +0900

    personal page 수정

--- frontend/src/components/PersonalPage/DiaryListView.vue
+++ frontend/src/components/PersonalPage/DiaryListView.vue
@@ -4,51 +4,47 @@
         <!--로그인한 사람이 계정 주인이라면 다 보여주고, 아니라면 공개 설정 된 글만 보여주기!!!!!!-->
         <v-card v-for="i in 10" :key="i" class="mx-auto" style="margin-top: 20px;">
             <v-container>
-                <v-card-title class="d-title">
+                <div>
+                    <div class="d-flex justify-space-between" flat>
+                      <div class="pa-2" outlined tile>
+                        2019.10.25
+                      </div>
+                    <div class="pa-2" outlined tile>
+                        <v-btn icon>
+                            <v-icon>mdi-heart</v-icon>
+                        </v-btn>
 
-                    디데이, title, 아기이름, 작성한 날짜
+                        <v-btn icon>
+                            <v-icon>mdi-message-text</v-icon>
+                        </v-btn>
+
+                        <v-btn icon>
+                            <v-icon>mdi-share-variant</v-icon>
+                        </v-btn>
+                      </div>
+                    </div>
+                  </div>
+                <v-card-title class="d-title">
+                    <div>D+234 ♥ 단비 | 육아...힘들다...</div>
                 </v-card-title>
               <v-row justify="space-between">
-                <v-col cols="auto">
+                <v-col>
                     <!-- 사진이 여러장인 경우 carousel 로 나타내주기-->
                   <v-img
                     height="200"
                     width="200"
-                    src="../../images/baby.png"
+                    src="../../images/Babies/danbi.jpg"
                   ></v-img>
                 </v-col>
                 <!--모바일 화면에서는 보여주지 않기 / 일정 글자 이상이면 자르기(상세페이지에서 전부 보여주기)-->
-                <v-col v-if="winWidth > 700" cols="auto">
+                <v-col v-if="winWidth > 700" >
 
-                  <div style="height:200px; max-width: 400px;">내용내용내영용내용</div>
-                </v-col>
-
-                <v-col
-                  cols="auto"
-                  class="text-center pl-0"
-                >
-                  <v-row
-                    class="flex-column ma-0 fill-height"
-                    justify="center"
-                  >
-                    <v-col class="px-0">
-                      <v-btn icon>
-                        <v-icon>mdi-heart</v-icon>
-                      </v-btn>
-                    </v-col>
-
-                    <v-col class="px-0">
-                      <v-btn icon>
-                        <v-icon>mdi-message-text</v-icon>
-                      </v-btn>
-                    </v-col>
-
-                    <v-col class="px-0">
-                      <v-btn icon>
-                        <v-icon>mdi-share-variant</v-icon>
-                      </v-btn>
-                    </v-col>
-                  </v-row>
+                  <div class="d-content" style="height:200px;;">
+                      단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야
+                  단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야
+                      단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야
+                        단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야단비꼬야
+                  </div>
                 </v-col>
               </v-row>
             </v-container>
@@ -91,4 +87,12 @@
         font-weight: bold;
 
     }
+    .d-content {
+        word-break: break-all;
+        white-space: normal;
+        text-overflow: ellipsis;
+        -o-text-overflow: ellipsis;
+        overflow: hidden;
+    }
+
 </style>
