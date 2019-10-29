8790934ce4f811f1ad286b91ecb85819d4386600
don2101 <don210198@gmail.com>
2019-10-29 19:21:15 +0900

    wsgi deploy 오타 수정

--- back-end/baby_project/wsgi/deploy.py
+++ back-end/baby_project/wsgi/deploy.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "baby_proejct.config.settings.deploy")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "baby_project.config.settings.deploy")
 
 application = get_wsgi_application()
