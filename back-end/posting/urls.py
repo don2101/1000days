49de0e67b118c849239d35df4cc4f040cf71c626
don2101 <don210198@gmail.com>
2019-10-30 18:14:18 +0900

    S1P1351023-72 feature: 유저 메인 피드 API 구축
    - 메인 피드 요청 기능 구현
    - 사용자가 작성한 글 + 사용자가 팔로우 하는 계정이 작성한 글을 반환
    - urls.py에 route 설정

--- back-end/posting/urls.py
+++ back-end/posting/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import post_diary, diary, post_image, user_diaries, like
+from .views import post_diary, diary, post_image, user_diaries, like, main_feed
 
 urlpatterns = [
     path('', post_diary, name="post_diary"),
     path('<int:diary_id>/images/', post_image, name="post_image"),
     path('<int:diary_id>/', diary, name="diary"),
     path('<str:account_name>/', user_diaries, name="diaries"),
+    path('<str:account_name>/main/', main_feed, name="main_feed"),
     path('<int:diary_id>/likes/', like, name="like")
 ]
