3854bdbbb0e0fe93fd50f46d8c888bf8f108f2d9
황유성 <ghdcjf9039@gmail.com>
2019-10-30 10:24:12 +0900

    getusers API 구현 중
    - url 추가 및 순서 변경
    - getusers API 구현 중

--- back-end/account/urls.py
+++ back-end/account/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
-from .views import signup, login, personal, babies, follow, logout, authuser, like, profile_image
+from .views import signup, login, personal, babies, follow, logout, authuser, like, profile_image, getusers
 
 
 urlpatterns = [
     path('signup/', signup, name="signup"),
     path('login/', login, name="login"),
+    path('getusers/', getusers, name="getusers"),
+    path('logout/', logout, name="logout"),
+    path('authuser/', authuser, name="authuser"),
     path('<str:account_name>/', personal, name="personal"),
     path('<str:account_name>/babies/', babies, name="babies"),
     path('<str:account_name>/follow/', follow, name="follow"),
-    path('logout/', logout, name="logout"),
-    path('authuser/', authuser, name="authuser"),
     path('<str:account_name>/likes/', like, name="like"),
     path('<str:account_name>/profile_image/', profile_image, name="profile_image")
 ]
