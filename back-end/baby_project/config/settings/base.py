2fb0f4722b5f5cb62024786536ae558c361a0b2b
don2101 <don210198@gmail.com>
2019-10-31 13:56:04 +0900

    database 옵션 추가

--- back-end/baby_project/config/settings/base.py
+++ back-end/baby_project/config/settings/base.py
@@ -79,7 +79,11 @@ DATABASES = {
         'PASSWORD': DB_PW,
         'HOST': '127.0.0.1',
         'PORT': '3306'
-    }
+    },
+
+    'OPTIONS': {
+        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+    },
 }
 
 
